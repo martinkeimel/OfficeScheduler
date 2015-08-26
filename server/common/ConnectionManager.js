7ac81f8efd5778e371ed409afa49ce7d2bd5fa54
martinkeimel <martinkeimel@gmail.com>
2015-08-26 11:53:12 -0300

    Change mongolab db connection

--- server/common/ConnectionManager.js
+++ server/common/ConnectionManager.js
@@ -4,7 +4,7 @@ var db = null;
 exports.Connect = function(){
 	if (db == null){
 		//mongoose.connect('mongodb://localhost:27017/RoomBooker');
-		mongoose.connect('mongodb://roombooker:roombooker123@dbh13.mongolab.com:27137/roombooker');
+		mongoose.connect('mongodb://roombooker:roombooker123@ds033153.mongolab.com:33153/roombookeraz');
 		db = mongoose.connection;
 		db.on('error', console.error.bind(console, 'connection error:'));
 	}	
